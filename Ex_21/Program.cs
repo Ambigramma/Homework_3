65153ffe8e090577b199bc392b43bfaa62ed5621
Andres Karapetyan <illum.andres@gmail.com>
2022-12-25 20:21:32 +0400

    Task21

---
+++ Ex_21/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+
+static void Space()
+{
+    double a1 = double.Parse(Console.ReadLine());
+    double a2 = double.Parse(Console.ReadLine());
+    double a3 = double.Parse(Console.ReadLine());
+    double b1 = double.Parse(Console.ReadLine());
+    double b2 = double.Parse(Console.ReadLine());
+    double b3 = double.Parse(Console.ReadLine());
+    
+    double ab = Math.Round(Math.Sqrt((Math.Pow(a1-b1, 2))+(Math.Pow(a2-b2, 2))+(Math.Pow(a3-b3, 2))), 2);
+    
+    Console.WriteLine(ab);
+}
+
+Space();
+
+
