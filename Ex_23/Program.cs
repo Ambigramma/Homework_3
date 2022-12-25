2707c2f1a47260d14b9231d22f0faab10cc3d8c8
Andres Karapetyan <illum.andres@gmail.com>
2022-12-25 22:32:42 +0400

    Task23

---
+++ Ex_23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+static void Cub(int n)
+{
+    int count = 1;
+
+    while(count <= n)
+    {
+        Console.WriteLine(Math.Pow(count, 3));
+        count++;
+    }
+    
+}
+
+int n = int.Parse(Console.ReadLine());
+Cub(n);
